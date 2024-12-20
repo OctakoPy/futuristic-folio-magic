beeabd33edd7ca1f021fc826e5834304f3814d73
OctakoPy <octakopy@gmail.com>
2024-12-20 23:07:10 +0800

    attempt pages 1

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: "/futuristic-folio-magic/"
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
